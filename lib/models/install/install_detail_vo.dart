d9a3c73e67d4aca55bbc25eff96fcc8e120059dc
Leezb101 <leezb101@126.com>
2025-07-25 18:58:22 +0800

    feat: 更新 InstallDetailVo 文档注释，添加未来字段或方法的占位说明

--- lib/models/install/install_detail_vo.dart
+++ lib/models/install/install_detail_vo.dart
@@ -2,11 +2,12 @@
  * @Author: LeeZB
  * @Date: 2025-07-25 18:55:34
  * @LastEditors: Leezb101 leezb101@126.com
- * @LastEditTime: 2025-07-25 18:56:15
+ * @LastEditTime: 2025-07-25 18:57:45
  * @copyright: Copyright © 2025 高新供水.
  */
 import 'package:pipe_code_flutter/models/install/do_install_vo.dart';
 
+/// [InstallDetailVo] extends [DoInstallVo] and serves as a placeholder for future fields or methods specific to installation details.
 class InstallDetailVo extends DoInstallVo {
   const InstallDetailVo({
     required super.materialList,
